8c714465a33491fff80f1867bf2048dd4fd1e91a
schop1schop@gmail.com <schop1schop@gmail.com>
2022-10-28 18:06:41 +0500

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react';
+import p5 from 'p5'
 
 interface IProps {
   api?: string
 }
 
 const Home: NextPage = (props: IProps) => {
+
+  useEffect(function() {
+    const script = document.createElement('script');
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.5.0/p5.min.js";
+    script.onload = function() {
+      console.log('loaded');
+    }
+    document.body.appendChild(script)
+
+    if (p5) {
+      const fft = new p5.FFT();
+      console.log(fft);
+    }
+    
+  }, [])
+
+
   return (
     <div>
       <img src="rat.jpg" alt="rat" />
-      <audio src="rat_music.mp3" autoPlay loop controls></audio>
+      <audio id='music' src="rat_music.mp3" autoPlay loop controls></audio>
     </div>
   )
 }
